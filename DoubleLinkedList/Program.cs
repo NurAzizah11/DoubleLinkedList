7dbbd27d41251376c8d938c5b48a373a0873bca6
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-16 16:42:15 +0700

    menambahkan Checks id the list is empty

--- DoubleLinkedList/Program.cs
+++ DoubleLinkedList/Program.cs
@@ -31,7 +31,23 @@ namespace DoubleLinkedList
             Node newnode = new Node();
             newnode.rollNumber = rollNo;
             newnode.name = nm;
-            
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll number not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+       
+            }
 
         }
     }
